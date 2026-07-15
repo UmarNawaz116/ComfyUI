731ab6ea51fdf05dc1d9076f718760c4a5a908fa
UmarNawaz116 <muhammadumarnawaz116@gmail.com>
2026-07-15 14:38:40 +0500

    fix: update startup batch files to target bundled python

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+..\Python310\python.exe main.py --force-fp16 --lowvram --enable-manager --auto-launch
+pause
---
+++ run_nvidia_gpu.bat
@@ -0,0 +1,3 @@
+@echo off
+..\Python310\python.exe main.py --force-fp16 --enable-manager --auto-launch
+pause
